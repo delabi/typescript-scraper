177e7fc921d732bd9872253dff1bfc9ae5ae1aba
Laban Bwire <bwire.laban@gmail.com>
2023-01-20 05:57:50 +0300

    refactorDRY

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,31 @@ import axios from "axios";
 import cheerio from "cheerio";
 import { createObjectCsvWriter } from "csv-writer";
 
-// const url = "https://www.lavuelta.es/en/rankings/stage-4";
-
-const url = "https://www.jumia.co.ke/single-pack-sugar/";
-
-const urlUg = "https://www.jumia.ug/sugar-3999/";
-
-const urlNigeria = "https://www.jumia.com.ng/sugars/";
+const urls = [
+  {
+    url: "https://www.jumia.co.ke/single-pack-sugar/",
+    country: "Kenya",
+  },
+  {
+    url: "https://www.jumia.ug/sugar-3999/",
+    country: "Uganda",
+  },
+  {
+    url: "https://www.jumia.com.ng/sugars/",
+    country: "Nigeria",
+  },
+];
 
 const AxiosInstance = axios.create();
-const csvWriterKe = createObjectCsvWriter({
-  path: "./outputKe.csv",
-  header: [
-    { id: "name", title: "Name" },
-    { id: "price", title: "Price" },
-  ],
-});
 
-const csvWriterUg = createObjectCsvWriter({
-  path: "./outputUg.csv",
+const csvWriterSugar = createObjectCsvWriter({
+  path: "./outputSugar.csv",
   header: [
     { id: "name", title: "Name" },
     { id: "size", title: "Size" },
     { id: "price", title: "Price" },
-  ],
-});
-
-const csvWriterNg = createObjectCsvWriter({
-  path: "./outputNg.csv",
-  header: [
-    { id: "name", title: "Name" },
-    { id: "price", title: "Price" },
+    { id: "country", title: "Country" },
+    { id: "date", title: "Date" },
   ],
 });
 
@@ -40,88 +34,61 @@ interface sugarData {
   name: string;
   size: string;
   price: string;
+  country: string;
+  date: string;
 }
 
-AxiosInstance.get(urlUg)
-  .then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    // const isNormalSugar = (value) => {
+{
+  urls.map(({ url, country }) => {
+    AxiosInstance.get(url)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const sugarListing: sugarData[] = [];
 
-    // }
-    const sugarListing: sugarData[] = [];
+        const sugarName = $(".info")
+          .map((_, product) => {
+            const $product = $(product);
+            const title: string = $product.find(".name").text();
+            const nameArray = title.split(" -");
+            const name = nameArray[0];
+            let size = "";
+            if (title.toLowerCase().includes("1kg")) {
+              size = "1kg";
+            } else if (title.toLowerCase().includes("2kg")) {
+              size = "2kg";
+            } else if (title.toLowerCase().includes("5kg")) {
+              size = "5kg";
+            } else if (title.toLowerCase().includes("10kg")) {
+              size = "10kg";
+            } else if (title.toLowerCase().includes("25kg")) {
+              size = "25kg";
+            } else if (title.toLowerCase().includes("50kg")) {
+              size = "50kg";
+            }
+            const price: string = $product.find(".prc").text();
 
-    const sugarName = $(".info")
-      .map((_, product) => {
-        const $product = $(product);
-        const title: string = $product.find(".name").text();
-        const nameArray = title.split("-");
-        //console.log("NameArray");
-        // console.log(nameArray);
-        const name = nameArray[0];
-        const size = nameArray[1];
-        const price: string = $product.find(".prc").text();
-        sugarListing.push({ name, size, price });
-        //return {"name": name, "price": price};
+            const date: string = Date();
+
+            sugarListing.push({ name, size, price, country, date });
+          })
+          .toArray();
+
+        const filteredList = sugarListing.filter((obj) => {
+          return (
+            !obj.name.toLowerCase().includes("bundle") &&
+            obj.size.toLowerCase().includes("kg")
+          );
+        });
+        
+        console.log("Start Filtered List of Type:" + typeof filteredList);
+        console.log(filteredList);
+        console.log("End Filtered List");
+
+        csvWriterSugar
+          .writeRecords(filteredList)
+          .then(() => console.log("Written to Sugar Output file"));
       })
-      .toArray();
-
-    const PATTERN = "kg";
-    const filteredList = sugarListing.filter((obj) =>
-      obj.name.toLowerCase().includes(PATTERN)
-    );
-
-    console.log("Start Sugar Listing Array of Type: " + typeof sugarListing);
-    console.log(sugarListing[2].name.toLowerCase().includes("kg"));
-
-    console.log("Start Filtered List of Type:" + typeof filteredList);
-    console.log(filteredList);
-    console.log("End Filtered List");
-    console.log("Type of SugarNameObject: " + typeof sugarName);
-
-    //console.log(sugarName);
-    csvWriterUg
-      .writeRecords(filteredList)
-      .then(() => console.log("Written to Ug file"));
-  })
-  .catch(console.error);
-
-// AxiosInstance.get(urlNigeria)
-// .then((response) => {
-//   const html = response.data;
-//   const $ = cheerio.load(html);
-//   const sugarName = $(".info")
-//     .map((_, product) => {
-//       const $product = $(product);
-//       const name: string = $product.find(".name").text();
-//       const price: string = $product.find(".prc").text();
-//       return {"name": name, "price": price};
-//     })
-//     .toArray();
-//     console.log(sugarName);
-//     csvWriterNg.writeRecords(sugarName).then(() => console.log("Written to Ng file"));
-// })
-// .catch(console.error);
-
-// AxiosInstance.get(url)
-//   .then((response) => {
-//     const html = response.data;
-//     //console.log(html);
-//     const $ = cheerio.load(html);
-
-//     const sugarName = $(".info")
-//       .map((_, product) => {
-//         const $product = $(product);
-//         const name: string = $product.find(".name").text();
-//         const price: string = $product.find(".prc").text();
-//         return {"name": name, "price": price};
-//       })
-//       .toArray();
-//     console.log(sugarName);
-//     csvWriterKe.writeRecords(sugarName).then(() => console.log("Written to KE file"));
-
-//   })
-//   .catch(console.error);
-
-// pb: 7597143
-// acc: Rck391
+      .catch(console.error);
+  });
+}
