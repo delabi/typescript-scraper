8faf676683c4b8d99621daeb526a4767c00457f5
Laban Bwire <bwire.laban@gmail.com>
2023-01-22 06:36:01 +0300

    Interval set

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata";
 import axios from "axios";
 import cheerio from "cheerio";
-import { createObjectCsvWriter } from "csv-writer";
 
 import express from "express";
 import * as bodyParser from "body-parser";
@@ -41,11 +40,10 @@ interface sugarData {
 
 AppDataSource.initialize()
   .then(async () => {
-
     // create express app
     const app = express();
     app.use(bodyParser.json());
-    app.use(cors())
+    app.use(cors());
     app.get("/users", (req, res) => {
       res.set("Access-Control-Allow-Origin", "*");
       res.send({ msg: "This has CORS enabled 🎈" });
@@ -83,69 +81,73 @@ AppDataSource.initialize()
 
     // setup express app here
 
-    {
-      urls.map(({ url, country }) => {
-        AxiosInstance.get(url)
-          .then((response) => {
-            const html = response.data;
-            const $ = cheerio.load(html);
+    function intervalFunc() {
+      {
+        urls.map(({ url, country }) => {
+          AxiosInstance.get(url)
+            .then((response) => {
+              const html = response.data;
+              const $ = cheerio.load(html);
 
-            const sugarName = $(".info")
-              .map(async (_, product) => {
-                const $product = $(product);
-                const title: string = $product.find(".name").text();
-                const nameArray = title.split(" -");
-                const name = nameArray[0];
-                let size = "";
-                if (title.toLowerCase().includes("1kg")) {
-                  size = "1kg";
-                } else if (title.toLowerCase().includes("2kg")) {
-                  size = "2kg";
-                } else if (title.toLowerCase().includes("5kg")) {
-                  size = "5kg";
-                } else if (title.toLowerCase().includes("10kg")) {
-                  size = "10kg";
-                } else if (title.toLowerCase().includes("25kg")) {
-                  size = "25kg";
-                } else if (title.toLowerCase().includes("50kg")) {
-                  size = "50kg";
-                }
-                const price: string = $product.find(".prc").text();
+              const sugarName = $(".info")
+                .map(async (_, product) => {
+                  const $product = $(product);
+                  const title: string = $product.find(".name").text();
+                  const nameArray = title.split(" -");
+                  const name = nameArray[0];
+                  let size = "";
+                  if (title.toLowerCase().includes("1kg")) {
+                    size = "1kg";
+                  } else if (title.toLowerCase().includes("2kg")) {
+                    size = "2kg";
+                  } else if (title.toLowerCase().includes("5kg")) {
+                    size = "5kg";
+                  } else if (title.toLowerCase().includes("10kg")) {
+                    size = "10kg";
+                  } else if (title.toLowerCase().includes("25kg")) {
+                    size = "25kg";
+                  } else if (title.toLowerCase().includes("50kg")) {
+                    size = "50kg";
+                  }
+                  const price: string = $product.find(".prc").text();
 
-                const date: string = Date();
+                  const date: string = Date();
 
-                sugarListing.push({ name, size, price, country, date });
-              })
-              .toArray();
+                  sugarListing.push({ name, size, price, country, date });
+                })
+                .toArray();
 
-            const filteredList = sugarListing.filter((obj) => {
-              return (
-                !obj.name.toLowerCase().includes("bundle") &&
-                obj.size.toLowerCase().includes("kg")
-              );
-            });
+              const filteredList = sugarListing.filter((obj) => {
+                return (
+                  !obj.name.toLowerCase().includes("bundle") &&
+                  obj.size.toLowerCase().includes("kg")
+                );
+              });
 
-            filteredList.map(async (obj) => {
-              const { name, size, price, country, date } = obj;
+              filteredList.map(async (obj) => {
+                const { name, size, price, country, date } = obj;
 
-              await AppDataSource.manager
-                .save(
-                  AppDataSource.manager.create(Sugar, {
-                    name: name,
-                    size: size,
-                    price: price,
-                    country: country,
-                    date: date,
-                  })
-                )
-                .catch((err) => {
-                  console.error();
-                });
-            });
-          })
-          .catch(console.error);
-      });
+                await AppDataSource.manager
+                  .save(
+                    AppDataSource.manager.create(Sugar, {
+                      name: name,
+                      size: size,
+                      price: price,
+                      country: country,
+                      date: date,
+                    })
+                  )
+                  .catch((err) => {
+                    console.error();
+                  });
+              });
+            })
+            .catch(console.error);
+        });
+      }
     }
+
+    setInterval(intervalFunc, 1000 * 60 * 60 * 6); // 6 hours interval
 
     // start express server
     app.listen(PORT, () => {
